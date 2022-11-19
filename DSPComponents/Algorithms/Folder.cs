b2764a0b1d18283648c7856ba0aab0e737f14145
Fares3993 <84674642+Fares3993@users.noreply.github.com>
2022-11-19 22:11:15 +0200

    Folding and Shifting

--- DSPComponents/Algorithms/Folder.cs
+++ DSPComponents/Algorithms/Folder.cs
@@ -11,10 +11,19 @@ namespace DSPAlgorithms.Algorithms
     {
         public Signal InputSignal { get; set; }
         public Signal OutputFoldedSignal { get; set; }
-
+        public static int FoldedCount = 0;
         public override void Run()
         {
-            throw new NotImplementedException();
+            FoldedCount++;
+            List<float> Samples = new List<float>();
+            List<int> NewIndex = new List<int>();
+            int count = InputSignal.Samples.Count;
+            for(int i = 1; i <= count; i++)
+            {
+                Samples.Add(InputSignal.Samples[count-i]);
+                NewIndex.Add(InputSignal.SamplesIndices[count - i] * -1);
+            }
+            OutputFoldedSignal = new Signal(Samples, NewIndex, false);
         }
     }
 }
--- DSPComponents/Algorithms/Shifter.cs
+++ DSPComponents/Algorithms/Shifter.cs
@@ -12,10 +12,21 @@ namespace DSPAlgorithms.Algorithms
         public Signal InputSignal { get; set; }
         public int ShiftingValue { get; set; }
         public Signal OutputShiftedSignal { get; set; }
-
+        
+        public int Fsign = 1;
         public override void Run()
         {
-            throw new NotImplementedException();
+            List<int> NewIndex = new List<int>();
+            if (/*Folder.FoldedCount % 2 != 0*/InputSignal.Samples[0] == 1)
+                Fsign = -1;
+            //else if (Folder.FoldedCount % 2 == 0)
+            //    Fsign = 1;
+            for (int i = 0; i < InputSignal.Samples.Count; i++)
+            {
+                InputSignal.SamplesIndices[i] = InputSignal.SamplesIndices[i] + (ShiftingValue*Fsign);
+                NewIndex.Add(InputSignal.SamplesIndices[i]);
+            }
+            OutputShiftedSignal = new Signal(InputSignal.Samples, NewIndex, false);
         }
     }
 }
