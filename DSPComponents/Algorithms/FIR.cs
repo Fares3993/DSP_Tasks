20126217fe859430cded9fbbf6bb969a2e98c824
ali01 <ali.01119544719@gmail.com>
2022-12-10 00:13:54 +0200

    added the task files

--- DSPComponents/Algorithms/FIR.cs
+++ DSPComponents/Algorithms/FIR.cs
@@ -22,7 +22,69 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            List<float> inputs = InputTimeDomainSignal.Samples;
+            List<float> idealResponses = new List<float>(); // Hd(n)
+            List<float> impulseResponses = new List<float>(); // W(n)
+
+            for(int i = 0; i < inputs.Count; i++)
+            {
+                inputs[i] = inputs[i] / InputFS;
+                idealResponses.Add(IdealResponse(i));
+                //impulseResponses.Add(ImpulseResponse(i));
+            }
+            OutputHn = new Signal(idealResponses, false);
+        }
+
+        private float IdealResponse(int n)
+        {
+            if (InputFilterType == FILTER_TYPES.LOW)
+            {
+                float cutOff = (float)(InputCutOffFrequency + (InputTransitionBand / 2));
+                if (n == 0)
+                {
+                    return 2 * cutOff;
+                }
+                else
+                {
+                    return (float)
+                        (2 * cutOff * Math.Sin(n * 2 * Math.PI * cutOff) 
+                        / (n * 2 * Math.PI * cutOff));
+                }
+            }
+            else if (InputFilterType == FILTER_TYPES.HIGH)
+            {
+                float cutOff = (float)(InputCutOffFrequency + InputTransitionBand / 2);
+                if (n == 0)
+                {
+                    return 1 - (2 * cutOff);
+                }
+                else
+                {
+                    return (float)
+                        (-2 * cutOff * Math.Sin(n * 2 * Math.PI * cutOff) 
+                        / (n * 2 * Math.PI * cutOff));
+                }
+            }
+            return 0;
+        }
+
+        private float ImpulseResponse(int n)
+        {
+            float TransitionWidth = InputTransitionBand / InputFS;
+
+            if (InputStopBandAttenuation < 21)
+            {
+                return 1;
+            }
+            else if (InputStopBandAttenuation < 53)
+            {
+                float N = (float)Math.Round(3.3 / TransitionWidth);
+
+                if (N % 2 == 0)
+                    N++;
+                return (float)(0.54 + 0.64 * Math.Cos(2 * Math.PI * n / N));
+            }
+            return 0;
         }
     }
 }
